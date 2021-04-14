2e86e6233d3b52bad8b91e8046156890b6edf0ea
Harold Waters <harold.waters@flosports.tv>
2021-04-13 21:58:51 -0500

    chore(examples): better typing the publish message example

--- examples/client/publish-message.ts
+++ examples/client/publish-message.ts
@@ -1,16 +1,16 @@
 import { ClientGooglePubSub } from '../../';
 
-const topicName: string = process.argv[2];
-const message: string = process.argv[3];
+const topicName: string | undefined = process.argv[2];
+const message: string | undefined = process.argv[3];
 
 function printHelp() {
     console.log(`Usage: ${process.argv0} ${process.argv[1]} TOPIC_NAME MESSAGE`);
 }
 
-function publishMessage() {
+function publishMessage(topic: string, message: string) {
     const client = new ClientGooglePubSub();
 
-    return client.publishToTopic(topicName, Buffer.from(message)).toPromise();
+    return client.publishToTopic(topic, Buffer.from(message)).toPromise();
 }
 
 if (!topicName || !message) {
@@ -18,7 +18,7 @@ if (!topicName || !message) {
     process.exit(1);
 }
 
-publishMessage()
+publishMessage(topicName, message)
     .then(() => console.log(`Message published to ${topicName}`))
     .catch((error) => console.log(`An error ocurred while publishing: ${error.message}`))
     .finally(() => process.exit(0));
