3d2e8e31a36aeeccb15fd166f48a80d4a2b356a6
Harold Waters <harold.waters@flosports.tv>
2021-04-15 09:26:26 -0500

    feat(body decorator): add a property parameter to the body decorator
    Allow the user to specify a propery on the body to retrieve

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,6 @@ export * from './client';
 export * from './ctx-host';
 export * from './decorators';
 export * from './deserializers';
+export * from './interfaces';
 export * from './serializers';
 export * from './server';
