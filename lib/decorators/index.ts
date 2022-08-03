22857289b0a755c089eaea0418c85d13c72b96bb
Harold Waters <harold.waters@flosports.tv>
2022-08-03 15:38:42 -0500

    feat: add @GooglePubSubMessageDeliveryId decorator

--- lib/decorators/index.ts
+++ lib/decorators/index.ts
@@ -4,3 +4,4 @@ export { GooglePubSubMessageBody } from './google-pubsub-message-body.decorator'
 export { GooglePubSubMessageMessageAttributes } from './google-pubsub-message-attributes.decorator';
 export { GooglePubSubMessageHandler } from './google-pubsub-message-handler.decorator';
 export { GooglePubSubMessageDeliveryAttempts } from './google-pubsub-delivery-attempts.decorator';
+export { GooglePubSubMessageId } from './google-pubsub-message-id.decorator';
