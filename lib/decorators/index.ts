53be8dc1df1ebe2c6ddcaee195bb946be31afd80
Harold F. Waters <harold.waters@flosports.tv>
2022-08-03 11:08:00 -0500

    feat: add @GooglePubSubMessageDeliveryAttempts decorator
    * Add a new decorator to expose the number of delivery attempts for
      the received message

--- lib/decorators/index.ts
+++ lib/decorators/index.ts
@@ -3,3 +3,4 @@ export { GooglePubSubMessageNack } from './google-pubsub-nack.decorator';
 export { GooglePubSubMessageBody } from './google-pubsub-message-body.decorator';
 export { GooglePubSubMessageMessageAttributes } from './google-pubsub-message-attributes.decorator';
 export { GooglePubSubMessageHandler } from './google-pubsub-message-handler.decorator';
+export { GooglePubSubMessageDeliveryAttempts } from './google-pubsub-delivery-attempts.decorator';
